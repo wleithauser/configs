817f7ee86dcfb55aee4edc45c5c7c1cdf087a209
William Leithauser <leithawj@mail.uc.edu>
2020-12-12 20:54:12 -0500

    Updatse

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,4 +1,6 @@
 c.hints.chars = "arstdhneio"
 c.qt.args = ["ppapi-widevine-path=/usr/lib/qt/plugins/ppapi/libwidevinecdmadapter.so"]
 c.tabs.show = "multiple"
+config.bind(',m', 'spawn mpv {url}')
+config.bind(',M', 'hint links spawn mpv {url}')
 config.source('nord-qutebrowser.py')
